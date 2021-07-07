df0109eec1d588c9a9e63cd8234392e01803eb46
Emil Olejnik <olejnike2000@gmail.com>
2021-07-07 15:40:49 +0200

    added delay to links

--- js/main.js
+++ js/main.js
@@ -4,9 +4,20 @@ const links = nav.querySelectorAll('.nav__item');
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav--active');
+    handleNavItemsAnimation();
 });
 links.forEach(item => {
     item.addEventListener('click', () => {
         nav.classList.remove('nav--active');
     });
 });
+
+const handleNavItemsAnimation = () =>
+{
+    let delay = 0;
+    links.forEach(item => {
+        item.classList.toggle('nav-items-animation');
+        item.style.animationDelay = '.' + delay + 's';
+        delay++;
+    });
+}
