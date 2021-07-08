67b4a75125c4b488ebc713fc0716ba905f83916e
Emil Olejnik <olejnike2000@gmail.com>
2021-07-08 17:56:17 +0200

    Added script to bars; finished project

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 const nav = document.querySelector('.nav');
 const burger = document.querySelector('.burger-btn');
 const links = nav.querySelectorAll('.nav__item');
+const navbars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav--active');
+    changeBarsColor();
     handleNavItemsAnimation();
 });
 links.forEach(item => {
@@ -21,3 +25,35 @@ const handleNavItemsAnimation = () =>
         delay++;
     });
 }
+
+const handleCurrentYear = () => 
+{
+    let year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+const handleObserver = () => 
+{
+    const currentSection = window.scrollY;
+    allSections.forEach(section => {
+
+        if(section.classList.contains('white-section') && section.offsetTop <= currentSection + 60)
+        {
+            navbars.classList.add('black-bars-color');
+        }
+        else if(!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60)
+        {
+            navbars.classList.remove('black-bars-color');
+        }
+
+    });
+}
+const changeBarsColor = () =>
+{
+    if(navbars.classList.contains('black-bars-color'))
+    {
+        navbars.classList.remove('black-bars-color');
+    }
+}
+window.addEventListener('scroll', handleObserver);
+handleCurrentYear();
